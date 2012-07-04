768f223cfb9f593c50c392313979116c2d78f708
ShiftyAxel <jpalmerwatkins@me.com>
2012-07-04 22:35:53 +0200

    Final, final final version. Tested and working. Honest.

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-rsync -tr --delete /srv/share/LunaSysMgr .
+mkdir -p ./LunaSysMgr
+rsync -trv --delete /srv/share/LunaSysMgr ./
 cd ./woce-build
 make custom
-rsync ./packages/sysmgr/luna-sysmgr/build/custom/armv7-stage/release-topaz/ /srv/share/release-topaz
+cd ../
+mkdir -p /srv/share/release-topaz
+rsync -trv --delete ./woce-build/packages/sysmgr/luna-sysmgr/build/custom/armv7-stage/release-topaz/ /srv/share/release-topaz
+rsync -trv --delete ./LunaSysMgr /srv/share/
