128c27a20604d1cc40907166c0a7df3c7ddf4795
ShiftyAxel <jpalmerwatkins@me.com>
2012-07-04 16:04:55 +0200

    Updated build.sh to properly remove files before and after copying

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-cp /srv/share/LunaSysMgr ./
+rm -rf ./LunaSysMgr
+cp -r /srv/share/LunaSysMgr ./
 cd woce-build
 make custom
 mkdir -p /srv/share/build
+rm -rf /srv/share/release-topaz
 cp -r packages/sysmgr/luna-sysmgr/build/woce/armv7-stage/release-topaz/ /srv/share/
 rm -rf ./LunaSysMgr
