4034abf42c7aba04525faece018caa5fbb6ed86c
Josh Palmer <jpalmerwatkins@me.com>
2012-07-05 20:23:56 +0100

    Remove outdated resources

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p ./LunaSysMgr
-rsync -trv --delete /srv/share/LunaSysMgr ./
-cd ./woce-build
-make custom
-cd ../
-mkdir -p /srv/share/release-topaz
-rsync -trv --delete ./woce-build/packages/sysmgr/luna-sysmgr/build/custom/armv7-stage/release-topaz/ /srv/share/release-topaz
-rsync -trv --delete ./LunaSysMgr /srv/share/
